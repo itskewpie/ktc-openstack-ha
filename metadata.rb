3012c0d1d0eb5ac6e75c399b529df3eff5b5ce67
Jesse Nelson <spheromak@gmail.com>
2013-08-19 18:43:46 -0700

    initial conversion over to App cook

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,10 @@ maintainer        "KT Cloudware, Inc."
 maintainer_email  "chamankang@kt.com"
 description       "Wrapper cookbook for rcb's openstack-ha"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.5"
+version           "1.0.6"
 supports          "ubuntu"
 
-depends "openstack-ha"
+depends "haproxy"
+depends "keepalived"
 
 recipe "default", ""
