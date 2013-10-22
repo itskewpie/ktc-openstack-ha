e4e4752923729ece1bf1019f7a4c8da0f3f21ecc
Wil Reichert <wil.reichert@kt.com>
2013-10-22 15:53:27 +0900

    cleanup example

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,16 +2,6 @@ include_attribute "ktc-utils"
 
 default[:vips] = {
   # overide this example
-  example: {
-    ip: "127.0.0.1",
-    port: 1234,
-    options: %w[ some option settings ],
-    proto: "udp",
-    mode: "http",
-    algo: "sh",
-    kind:  "tun",
-    net:  "public"
-  },
   "mysql" => {
     ip: "10.0.2.50",
     port: 3306
