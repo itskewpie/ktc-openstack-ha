4da0d9e3541c5f4e1b176530dfa8a2628b9329ca
Wil Reichert <wil.reichert@kt.com>
2013-10-30 21:20:38 +0900

    fix ipc-stage environment name

---
+++ attributes/ipc-stage.rb
@@ -0,0 +1,11 @@
+# setup ipc_stag vips
+return unless chef_environment == "ipc-stage"
+# we want to override defaults
+include_attribute "ktc-openstack-ha::default"
+
+default[:vips] = {
+ "tags" => {
+    mysql: "10.9.241.35",
+    api: "10.9.241.36"
+  }
+}
