f98d67d4309c10f07c0ec02d385c38d74df89915
Jenk <jenkins.ktcloudware@gmail.com>
2013-10-30 15:13:16 +0900

    auto_version to 2.0.6

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "KT Cloudware, Inc."
 maintainer_email  "chamankang@kt.com"
 description       "Wrapper cookbook for rcb's openstack-ha"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.0.5'
+version '2.0.6'
 supports          "ubuntu"
 
 depends "haproxy"
