d7c044db0785b82be8e7c6747d462eeac662b3b4
chamankang <chaman.kang@kt.com>
2013-11-15 16:43:43 +0900

    fix wrong vip tag

--- attributes/ipc-stage.rb
+++ attributes/ipc-stage.rb
@@ -4,6 +4,7 @@ return unless chef_environment == "ipc-stage"
 include_attribute "ktc-openstack-ha::default"
 
 default[:vips][:tags] = {
+  rabbitmq: "10.9.241.34",
   mysql: "10.9.241.35",
   api: "10.9.241.36"
 }
