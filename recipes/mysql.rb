7cdbdddcc2b7a3458641e1f9d553675b864691fb
Jesse Nelson <spheromak@gmail.com>
2013-11-14 19:59:23 -0800

    fix run_state key name

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -1,4 +1,4 @@
-node.run_state[:active_passive] ||= Array.new
-node.run_state[:avtive_passive].push :mysql
+node.run_state[:active_passive] ||= []
+node.run_state[:active_passive].push :mysql
 
 include_recipe "ktc-openstack-ha::_active_passive"
--- recipes/rabbitmq.rb
+++ recipes/rabbitmq.rb
@@ -1,4 +1,4 @@
-node.run_state[:active_passive] ||= Array.new
-node.run_state[:avtive_passive].push :rabbitmq
+node.run_state[:active_passive] ||= []
+node.run_state[:active_passive].push :rabbitmq
 
 include_recipe "ktc-openstack-ha::_active_passive"
